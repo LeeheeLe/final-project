2b0c6f54eb619a51e3590341ffa7bb5d4e7e1e0b
LeeheeLe <leeheele.h@gmail.com>
2025-02-14 17:14:25 +0200

    fixed typos and made function names uniform

--- preprocessor.h
+++ preprocessor.h
@@ -1,17 +1,15 @@
-#ifndef PREPROCCESOR_H
-#define PREPROCCESOR_H
+#ifndef PREPROCESSOR_H
+#define PREPROCESSOR_H
 #define MACRO_START "mcro"
 #define MACRO_END "mcroend"
 
 #define PREPROCESSED_EXT ".as"
 #define INPUT_EXT ".am"
 
-
 #include "errors.h"
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
-
 
 struct line{
     char* line;
@@ -24,13 +22,12 @@ struct macro_table {
 };
 
 void preprocess(const char *input_file, const char *output_file);
-int mcrostart(const char *line);
-int mcroend(const char *line, error_code *ecode, const int line_number);
-int isreserved(char *word);
+int mcro_start(const char *line);
+int mcro_end(const char *line, error_code *ecode, const int line_number);
+int is_reserved(char *word);
 void insert_macro_name(const char *line,struct macro_table *curr_macro, error_code *ecode, int line_number);
 int is_saved_macro(const char *line, struct macro_table *head, error_code *ecode);
 void print_macro_contents_to_file(const int macro_idx,struct macro_table *head_macro, FILE *output);
 void append_line_to_macro(char *line,struct macro_table *curr_macro);
 
-
-#endif //PREPROCCESOR_H
+#endif /*PREPROCESSOR_H*/
