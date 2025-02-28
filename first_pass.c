0b95d42fce5b81f857d1b12ad281ba7039fc8b4f
Itay Zimet <itay.zimet@gmail.com>
2025-02-28 10:58:13 +0200

    added small first parts of the first pass and some skeleton for the rest. organised file extension into a separate header file

--- first_pass.c
+++ first_pass.c
@@ -1,2 +1,102 @@
 #include "first_pass.h"
 
+int is_whitespace(const char *line);
+int is_comment(const char *line);
+
+void first_pass(const char *file_name) {
+  int IC = 100, DC = 100;
+  char *input_file;
+  input_file = malloc(strlen(file_name) + strlen(INPUT_EXT) + 1);
+  if (input_file == NULL) {
+    MEM_ALOC_ERROR();
+    free(input_file);
+    return;
+  }
+  input_file = strcpy(input_file, file_name);
+  strcat(input_file, INPUT_EXT);
+  FILE *input = fopen(input_file, "r"); /*open input file in 'read' mode*/
+  free(input_file);
+  if (input == NULL) {
+    FILE_OPEN_ERROR();
+    return;
+  }
+  char *line, *work_line, *label_name;
+  enum instruction {
+    DATA,
+    STRING,
+    ENTRY,
+    EXTERN
+  } instruction_type; // todo: extract to different file
+  int line_number = 0;
+  int label_flag = 0;
+  while ((work_line = line = getLine(input)) != NULL) {
+    line_number++;
+    if (is_whitespace(line) || is_comment(line)) {
+      continue;
+    }
+    if (is_label(&work_line, &label_name)) {
+      label_flag = 1;
+    }
+    /*if (is_data_instruction(line, &instruction_type)) {
+        if (label_flag) {
+            insert_label();//todo: figure out what are the correct params here
+        }
+        //todo: find data type, encode it and increase DC accordingly
+        continue;
+    }
+    if (is_linking_instruction(line, &instruction_type)) {
+        if (instruction_type == EXTERN) {
+            insert_label();// todo: figure out correct params here
+        }
+        continue;
+    }
+    if (label_flag) {
+        insert_label();//todo: figure out correct params
+    }*/
+
+    free(line);
+  }
+}
+
+int is_whitespace(const char *line) {
+  if (strlen(line) == 0) {
+    return 1;
+  }
+  while (*line != '\0' && isspace(*line)) {
+    line++;
+  }
+  return *line == '\0';
+}
+
+int is_comment(const char *line) {
+  if (*line == ';') {
+    return 1;
+  }
+  return 0;
+}
+
+int is_label(char **line, char **label_name) {
+  int label_length = 0;
+  char *name = malloc(32 * sizeof(char)), *work_line = *line;
+  while (isspace(*work_line)) {
+    work_line++;
+  }
+  if (isalpha(*work_line)) {
+    *name = *work_line;
+    work_line++;
+    name++;
+    label_length++;
+  } /*checks that first character is a letter*/
+  for (; !isspace(*work_line); work_line++) {
+    if (*work_line == LABEL_DEF_CHAR) {
+      *name = '\0';
+      *label_name = name - label_length;
+      *line = ++work_line;
+      return 1;
+    }
+    *name = *work_line;
+    name++;
+    label_length++;
+  }
+  return 0;
+}
--- first_pass.h
+++ first_pass.h
@@ -1,4 +1,18 @@
 #ifndef FIRST_PASS_H
 #define FIRST_PASS_H
 
-#endif //FIRST_PASS_H
+#include "errors.h"
+#include "file_extensions.h"
+#include "input.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LABEL_DEF_CHAR ':'
+#define INPUT_EXT PREPROCESSOR_OUTPUT_EXT
+/*the input of the first pass is the output of the preprocessor*/
+
+void first_pass(const char *file_name);
+int is_label(char **line, char **label_name);
+
+#endif // FIRST_PASS_H
