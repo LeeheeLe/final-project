3f4da785b3be8e8b3c86ae9b1d80cf1748ab3ff7
itayzimet <itay.zimet@gmail.com>
2025-02-28 14:08:19 +0200

    refactoring for more efficient parsing

--- first_pass.h
+++ first_pass.h
@@ -9,30 +9,8 @@
 #include <string.h>
 #include <ctype.h>
 
-typedef enum {
-  DATA,
-  STRING,
-  ENTRY,
-  EXTERN,
-  INVALID
-} inst;
 
-typedef struct {
-  inst instruction;
-  char *name;
-} instruction;
 
-instruction instructions[] = {
-  {DATA, "data"},
-  {STRING, "string"},
-  {ENTRY, "entry"},
-  {EXTERN, "extern"}
-};
-
-#define NUMBER_OF_INSTRUCTION_TYPES 4
-
-#define LABEL_DEF_CHAR ':'
-#define INSTRUCTION_START_CHAR '.'
 
 #define INPUT_EXT PREPROCESSOR_OUTPUT_EXT
 /*the input of the first pass is the output of the preprocessor*/
---
+++ parsing.h
@@ -0,0 +1,36 @@
+
+
+#ifndef PARSING_H
+#define PARSING_H
+
+typedef enum {
+  DATA,
+  STRING,
+  ENTRY,
+  EXTERN,
+  INVALID
+} inst;
+
+typedef struct {
+  inst instruction;
+  char *name;
+} instruction;
+
+instruction instructions[] = {
+  {DATA, "data"},
+  {STRING, "string"},
+  {ENTRY, "entry"},
+  {EXTERN, "extern"}
+};
+#define NUMBER_OF_INSTRUCTION_TYPES 4
+
+
+#define LABEL_DEF_CHAR ':'
+#define INSTRUCTION_START_CHAR '.'
+
+int is_instruction(char **line, inst *instruction_type, int line_number);
+int is_whitespace(const char *line);
+int is_comment(const char *line);
+int is_label(char **line, char **label_name);
+
+#endif /*PARSING_H*/
