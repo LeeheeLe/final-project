17008f11cf505a73925b16eef79c60c0cdcd2240
itayzimet <itay.zimet@gmail.com>
2025-03-07 10:14:51 +0200

    organise errors and add a label conflict error

--- errors.h
+++ errors.h
@@ -1,12 +1,19 @@
 #ifndef ERRORS_H
 #define ERRORS_H
 
+/*Memory error macros*/
 #define MEM_ALOC_ERROR() printf("Error: memory allocation error.\n")
 #define FILE_OPEN_ERROR() printf("Error: failed to open file.\n")
+
+/*macro errors*/
 #define EXTRA_CHARS_MACRO_ERROR(line) printf("Error in line %d: extra chars after macro defenition.\n", line)
 #define MACRO_NAME_RESERVED(line) printf("Error in line %d: macro cannot have a reserved name.\n", line)
-#define INVALID_INSTRUCTION(line) printf("Error in line %d: invalid instruction name.\n", line)
 
+/*parsing errors*/
+#define INVALID_INSTRUCTION(line) printf("Error in line %d: invalid instruction name.\n", line)
+#define CONFLICTING_LABELS(line, label_name) printf("Error in line %d: label %s already exists.\n", line, label_name)
+
+/*warnings*/
 #define LABELED_LINKING_WARNING(line) printf("Warning in line %d: labeling a .extern or a .entry instruction has no meaning.\n", line)
 
 enum errors {NORMAL, ERROR};
