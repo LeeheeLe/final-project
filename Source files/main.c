096c21d850223757ff4c5760c96a9942785b614d
LeeheeLe <leeheele.h@gmail.com>
2025-04-02 20:48:30 +0300

    documentation

--- Source files/main.c
+++ Source files/main.c
@@ -2,16 +2,39 @@
 #include "../Header files/preprocessor.h"
 #include "../Header files/first_pass.h"
 
+/*
+ * Purpose:
+ * The entry point of the program. This function processes the command-line arguments,
+ * performs preprocessing on each file, and then runs the first pass assembler process
+ * on the specified files.
+ *
+ * The program expects at least one argument (the file to process). If no arguments
+ * are provided, an error message is displayed.
+ *
+ * Functionality:
+ * - Processes each file specified in the command line arguments.
+ * - Calls the `preprocess` function on each file to handle preprocessing.
+ * - Calls the `first_pass` function to execute the first pass of the assembler on each file.
+ * //todo complete this documenting acording to the last things we finish
+*
+ * @param argc The number of command-line arguments.
+ * @param argv The array of command-line argument strings.
+ *
+ * @return int Returns 0 if successful, 1 if an error occurs (like missing file argument).
+ */
 int main(const int argc, char *argv[]) {
 
+    /* Check if there are fewer than 2 command-line arguments */
     if (argc < 2) {
-        printf("error: missing argument\n");
-        return 1;
+        printf("error: missing argument\n");  /* Display error if no file is given */
+        return 1;  /* Return error code */
     }
+
     int i;
+    /* Loop through each command-line argument (file to process) */
     for (i = 1; i < argc; i++) {
-        preprocess(argv[i]);
-        first_pass(argv[i]);
+        preprocess(argv[i]);  /* Call the preprocess function on the file */
+        first_pass(argv[i]);  /* Call the first pass assembler on the file */
     }
-    return 0;
+    return 0;  /* Return success code */
 }
